9f9a87b7c1beb8974eb0f3d38d6c83ddec79e3c2
User <dedsnoop505@gmail.com>
2023-02-27 18:01:41 +0200

    square

--- oop.cpp
+++ oop.cpp
@@ -7,4 +7,22 @@ int main()
     std::cout << "S = 3.14 * R**2" << std::endl;
     std::cout << "R: ";
     std::cin >> R;
+    std::cout << std::endl;
+
+    double S, a;
+    S = pow(a, 2);
+    std::cout << "S = a**2" << std::endl;
+    std::cout << "a: ";
+    std::cin >> a;
+    std::cout << std::endl;
+
+    double S, a, b;
+    S = a * b;
+    std::cout << "S = a**2" << std::endl;
+    std::cout << "a: ";
+    std::cin >> a;
+    std::cout << std::endl;
+    std::cout << "b: ";
+    std::cin >> b;
+    std::cout << std::endl;
 }
