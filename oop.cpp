45b76787fe1a47abc434551b593a7efb35d3a518
User <dedsnoop505@gmail.com>
2023-02-27 18:03:14 +0200

    rectangle

--- oop.cpp
+++ oop.cpp
@@ -7,4 +7,32 @@ int main()
     std::cout << "S = 3.14 * R**2" << std::endl;
     std::cout << "R: ";
     std::cin >> R;
+    std::cout << std::endl;
+
+    double S, a;
+    S = pow(a, 2);
+    std::cout << "S = a**2" << std::endl;
+    std::cout << "a: ";
+    std::cin >> a;
+    std::cout << std::endl;
+
+    double S, a, b;
+    S = a * b;
+    std::cout << "S = a*b" << std::endl;
+    std::cout << "a: ";
+    std::cin >> a;
+    std::cout << std::endl;
+    std::cout << "b: ";
+    std::cin >> b;
+    std::cout << std::endl;
+
+    double S, a, b;
+    S = (a * b)/2;
+    std::cout << "S = (a*h)/2" << std::endl;
+    std::cout << "a: ";
+    std::cin >> a;
+    std::cout << std::endl;
+    std::cout << "h: ";
+    std::cin >> b;
+    std::cout << std::endl;
 }
