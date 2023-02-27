2a8459c23ac71dd9617f7164e496814dfec08b8e
User <dedsnoop505@gmail.com>
2023-02-27 17:49:46 +0200

    Circle formula

--- oop.cpp
+++ oop.cpp
@@ -2,6 +2,9 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
-
+    double S, R;
+    S = 3.14 * pow(R, 2);
+    std::cout << "S = 3.14 * R**2" << std::endl;
+    std::cout << "R: ";
+    std::cin >> R;
 }
